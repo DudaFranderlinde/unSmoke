114b01e53cfaaf9406403dfd0ab04a8b72cb8636
DudaFranderlinde <108769434+DudaFranderlinde@users.noreply.github.com>
2022-07-28 11:03:04 -0300

    Primeiro commit

---
+++ app/src/main/java/com/example/unsmoke/TelaInicial.java
@@ -0,0 +1,41 @@
+package com.example.unsmoke;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+
+public class TelaInicial extends AppCompatActivity {
+
+    CheckBox checkMeta;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tela_inicial);
+        getWindow().setStatusBarColor(Color.BLACK);
+        getSupportActionBar().hide();
+
+        checkMeta = findViewById(R.id.checkMeta);
+    }
+
+    public void irTelaProgresso(View i){
+        Intent irTelaProgresso = new Intent(this, TelaProgresso.class);
+        startActivity(irTelaProgresso);
+    }
+
+    public void irPerfil(View ip){
+        Intent irTelaPerfil = new Intent(this, TelaPerfil.class);
+        startActivity(irTelaPerfil);
+    }
+
+    public void irRegistrarFumo(View ir){
+        Intent irTelaRegistroFumo = new Intent(this, TelaRegistroFumo.class);
+        startActivity(irTelaRegistroFumo);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/unsmoke/TelaProgresso.java
@@ -0,0 +1,108 @@
+package com.example.unsmoke;
+
+import static java.lang.Integer.parseInt;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import java.sql.Time;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class TelaProgresso extends AppCompatActivity {
+
+    String usuarioID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+    SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy"); //Formata
+    Date data = new Date(); // Pega a dataAtual
+
+    TextView diasNoApp, qtdCigarros;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tela_progresso);
+        getWindow().setStatusBarColor(Color.BLACK);
+        getSupportActionBar().hide();
+
+        diasNoApp = findViewById(R.id.diasNoApp);
+        qtdCigarros = findViewById(R.id.qtdCigarros);
+
+        qtdCigarros.setText(" ");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        DocumentReference documentReference = db.collection("Usuarios").document("Dados").collection(usuarioID).document("Informações pessoais");
+        documentReference.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException error) {
+                if (documentSnapshot != null){
+
+                    String data1 = documentSnapshot.getString("Data de cadastro");
+                    String data2 = sdf.format(data);
+
+                    try{
+                        Date primeiraDt = sdf.parse(data1);
+                        Date segundaDt = sdf.parse(data2);
+                        long diffEmMil = Math.abs(segundaDt.getTime() - primeiraDt.getTime());
+                        long diff = TimeUnit.DAYS.convert(diffEmMil, TimeUnit.MILLISECONDS);
+
+                        int data = (int) diff;
+                        diasNoApp.setText(data);
+                    }catch (ParseException e){
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        });
+    }
+
+    public void voltarParaTelaInicial(View v){
+        Intent voltarParaTelaInicial = new Intent (this, TelaInicial.class);
+        startActivity(voltarParaTelaInicial);
+    }
+
+    public void configCigarroFumados(){
+        // Falta: conectar variavel com BD e conseguir pegar o valor dele pra usar na comparação
+         int cigarros = Integer.parseInt(qtdCigarros.getText().toString());
+
+         if(cigarros <= 0 ){
+             qtdCigarros.setText("0");
+         }else{
+
+         }
+    }
+
+    public void calculaDinheiro(){
+        //Falta: conectar com o BD pra saber quais são os tipos, valor e qtd média de fumo diário para calcular uma média de dinheiro gasto
+
+        String tipoCigarro = null;//tive que inicar senão fica com erro
+        double valorMédio;
+        if(tipoCigarro.equals("Cigarro")){
+            valorMédio = 5.60;
+            double custoDia = valorMédio  /* * fumoDiario*/; // V
+            //double custoTotais = valorMédio * qtdCigarros;
+        }
+    }
+}
